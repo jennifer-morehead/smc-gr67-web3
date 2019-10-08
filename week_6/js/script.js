f3e2934ed144cdde63575df204baffe1b037738a
jennifer-morehead <52468606+jennifer-morehead@users.noreply.github.com>
2019-10-07 23:00:50 -0700

    js validation
    validate for state
    validate for numbers in zip

--- week_6/js/script.js
+++ week_6/js/script.js
@@ -20,25 +20,57 @@ function toggleBilling() {
 }
 
 
-
+var errorInput = document.getElementsByClassName('invalid-input');
 function alphaOnly() {
 	var fname = document.getElementById('shippingFirstName');
 	var lname = document.getElementById('shippingLastName');
 	var city = document.getElementById('shippingCity');
-	var errorNumbers = document.getElementsByClassName('invalid-input');
+	
 	var numbers = "^[0-9*#+@]+$";
 	if (fname.value.match(numbers) && document.activeElement == fname) {
-		errorNumbers[0].style.display = "block";
+		errorInput[0].style.display = "block";
 	} else if (lname.value.match(numbers) && document.activeElement == lname) {
-		errorNumbers[1].style.display = "block";
+		errorInput[1].style.display = "block";
 	} else if (city.value.match(numbers) && document.activeElement == city) {
-		errorNumbers[2].style.display = "block";
+		errorInput[3].style.display = "block";
 	} else {
-		errorNumbers[0].style.display = "none";
-		errorNumbers[1].style.display = "none";
-		errorNumbers[1].style.display = "none";
+		errorInput[0].style.display = "none";
+		errorInput[1].style.display = "none";
+		errorInput[3].style.display = "none";
 	}
 }
+
+function emailOnly() {
+	var email = document.getElementById('shippingEmail');
+	var emails = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+	if (email.value.match(emails)) {
+		errorInput[2].style.display ="none";
+	} else {
+		errorInput[2].style.display ="block";
+	}
+}
+function statesOnly() {
+	var state = document.getElementById('shippingState');	
+	var states = "^((A[LKZR])|(C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))$";
+	if (state.value.match(states)) {
+		errorInput[4].style.display = "none";
+	} else {
+		errorInput[4].style.display = "block";
+	}
+}
+
+
+function numbersOnly() {
+	var zip = document.getElementById('shippingZip');
+	var zips = "^[A-Za-z]+$" ;
+	if (zip.value.match(zips)) {
+		errorInput[5].style.display = "block";
+	} else {
+		errorInput[5].style.display = "none";
+	}
+}
+
+
 
 
 (function () {
