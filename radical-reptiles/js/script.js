b0ecc06f7fccfe0348b11832412371b3b23d4b7c
jennifer-morehead <52468606+jennifer-morehead@users.noreply.github.com>
2019-10-20 19:07:43 -0700

    Modals
    Program modal css and function
    Donate modal css only

--- radical-reptiles/js/script.js
+++ radical-reptiles/js/script.js
@@ -8,69 +8,6 @@
 */
 
 
-function toggleBilling() {
-	if (document.getElementById('customSwitches').checked == true) {
-		document.getElementById('billingGroup').style.display = "none";
-		document.getElementById('shippingGroup').classList.remove("col-md-5");
-	} 
-	else {
-		document.getElementById('billingGroup').style.display = "block";
-		document.getElementById('shippingGroup').classList.add("col-md-5");
-	}
-}
-
-
-var errorInput = document.getElementsByClassName('invalid-input');
-function alphaOnly() {
-	var fname = document.getElementById('shippingFirstName');
-	var lname = document.getElementById('shippingLastName');
-	var city = document.getElementById('shippingCity');
-	
-	var numbers = "^[0-9*#+@]+$";
-	if (fname.value.match(numbers) && document.activeElement == fname) {
-		errorInput[0].style.display = "block";
-	} else if (lname.value.match(numbers) && document.activeElement == lname) {
-		errorInput[1].style.display = "block";
-	} else if (city.value.match(numbers) && document.activeElement == city) {
-		errorInput[3].style.display = "block";
-	} else {
-		errorInput[0].style.display = "none";
-		errorInput[1].style.display = "none";
-		errorInput[3].style.display = "none";
-	}
-}
-
-function emailOnly() {
-	var email = document.getElementById('shippingEmail');
-	var emails = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
-	if (email.value.match(emails)) {
-		errorInput[2].style.display ="none";
-	} else {
-		errorInput[2].style.display ="block";
-	}
-}
-function statesOnly() {
-	var state = document.getElementById('shippingState');	
-	var states = "^((A[LKZR])|(C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))$";
-	if (state.value.match(states)) {
-		errorInput[4].style.display = "none";
-	} else {
-		errorInput[4].style.display = "block";
-	}
-}
-
-
-function numbersOnly() {
-	var zip = document.getElementById('shippingZip');
-	var zips = "^[A-Za-z]+$" ;
-	if (zip.value.match(zips)) {
-		errorInput[5].style.display = "block";
-	} else {
-		errorInput[5].style.display = "none";
-	}
-}
-
-
 
 
 (function () {
